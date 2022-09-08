8aaffa157e51d60481e6bcdfb87b84ce87fa0b70
Nahotjan <apieceofyellowcake@outlook.com>
2022-09-08 22:07:54 +0200

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+  name='dinoclib',
+  version='1.0',
+  description='Dinoc library',
+  author='nahotjan',
+  url='',
+  packages=['dinoclib'],
+  install_requires=['py7zr']
+)
